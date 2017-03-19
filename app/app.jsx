656ce324ec74bdf98f620893b3d0d52dc8dc3250
quyencao <boy20195@gmail.com>
2017-03-19 16:07:38 +0700

    Switch to scss

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // App css
-require('style!css!ApplicationStyles');
+require('style!css!sass!ApplicationStyles');
 
 ReactDOM.render(
     <Router history={hashHistory}>
